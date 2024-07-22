4a03b3d800d6d11da7774c4b657122876936ec58
Akshayredekar07 <akshayredekar1414@gmail.com>
2024-07-22 15:22:49 +0530

    Django ORM is Completed

--- ORMProject/testapp/views.py
+++ ORMProject/testapp/views.py
@@ -3,7 +3,7 @@ from testapp.models import Employee
 
 from django.db.models import Q
 from django.db.models import Aggregate, Avg, Min, Sum, Count, Max
-
+from django.db.models.functions import Lower
 
 # Create your views here.
 def retrive_view(request):
@@ -21,12 +21,19 @@ def retrive_view(request):
 
     # emp_list = Employee.objects.filter(ename__contains="d", esal__gt = 15000)
     # emp_list = Employee.objects.values_list('ename', 'esal', 'eaddr')
-    emp_list = Employee.objects.values('ename', 'esal', 'eaddr')
-    emp_list = Employee.objects.only('ename', 'esal', 'eaddr')
-
+    # emp_list = Employee.objects.values('ename', 'esal', 'eaddr')
+    # emp_list = Employee.objects.all()
+    # emp_list = Employee.objects.all().order_by('eno')
+    # emp_list = Employee.objects.all().order_by('-eno')
+    # emp_list = Employee.objects.all().order_by('ename')
+    # emp_list = Employee.objects.all().order_by(Lower('ename'))
+    q1 = Employee.objects.filter(esal__lt=15000)
+    q2 = Employee.objects.filter(ename__startswith="J")
+    q3 = q1.union(q2)
+    emp_list=q3
 
     # return render(request, 'testapp/index.html', {'emp_list': emp_list})
-    return render(request, 'testapp/specificcol.html', {'emp_list': emp_list})
+    return render(request, 'testapp/index.html', {'emp_list': emp_list})
 
 
 def aggregate_view(request):
@@ -38,7 +45,4 @@ def aggregate_view(request):
 
     my_dict={'avg':avg['esal__avg'], 'max':max['esal__max'], 'min':min['esal__min'], 'sum':sum['esal__sum'], 'count':count['esal__count']}
     return render(request, 'testapp/aggreegate.html', my_dict)
-
-
-
 
